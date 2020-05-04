4c6d5e140adae99b24d64abf6dbb2624c55c759f
Melanie Jindali <melaniejindali.dm@gmail.com>
2020-05-04 09:05:55 -0500

    Created switch case for commands

--- liribot.js
+++ liribot.js
@@ -3,5 +3,34 @@ var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
 var axios = require("axios");
 var moment = require("moment");
-var artist = process.argv[2];
+var fs = require("fs")
+
+var command = process.argv[2];
+var input = process.argv[3];
+
+var defaultSong = "The Sign";
+var defaultMovie = "Mr. Nobody";
+
+switch (command) {
+  case "concert-this":
+    getBands(input)
+    break;
+  case "spotify-this-song":
+    if (input === "") {
+      input = defaultSong;
+    }
+    getSongs(input)
+    break;
+  case "movie-this":
+    if (input == "") {
+      input = defaultMovie;
+    }
+    getMovies(input)
+    break;
+  case "do-what-it-says":
+    doWhatItSays()
+    break;
+  default:
+    break;
+}
 
