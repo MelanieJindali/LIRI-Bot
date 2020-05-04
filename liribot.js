aed2dc79be9ba0d5607812638ec7285cfbf763c2
Melanie Jindali <melaniejindali.dm@gmail.com>
2020-05-04 07:26:54 -0500

    Created access to keys

--- liribot.js
+++ liribot.js
@@ -1,1 +1,7 @@
 require("dotenv").config();
+var keys = require("./keys.js");
+var spotify = new Spotify(keys.spotify);
+var axios = require("axios");
+var moment = require("moment");
+var artist = process.argv[2];
+
